7b39b8e010887af65839c6997e8beef1e03ae98c
ManiSai <63541439+ManiSai18@users.noreply.github.com>
2024-01-28 01:23:11 -0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,3 @@
-echo " BUILD START"
-python -m pip install -r requirements.txt 
+echo "BUILD START"
+python -m pip install -r requirements.txt
 python manage.py collectstatic --noinput --clear
-echo " BUILD END"
